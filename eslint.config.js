91587f8441ed583d928cd3acbfd41b748bac51d6
Denis Lazarev <5255792+longbombus@users.noreply.github.com>
2026-09-17 18:39:53 +0300

    Add ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        document: 'readonly',
+        window: 'readonly',
+        fetch: 'readonly',
+        URLSearchParams: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
